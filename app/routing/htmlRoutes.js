ad391b9f0da3d931c83fd4a592286a3945d267ba
Josephine Strange <jostra@microsoft.com>
2019-01-19 10:02:57 -0800

    third commit

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,7 +7,7 @@ var path = require('path');
 
 var initializeapi = function(app){
 
-	app.get("/survey", function (req, res) {
+	app.get("/home", function (req, res) {
 		res.sendFile(path.join(__dirname ,  '../public/survey.html'))
 	
 		// If no matching route is found default to home
