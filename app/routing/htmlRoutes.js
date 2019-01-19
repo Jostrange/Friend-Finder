96e12d933c56bec4a5e5375183ca3ed647b0679c
Josephine Strange <jostra@microsoft.com>
2019-01-19 13:04:24 -0800

    third commit

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,18 +5,20 @@
 var path = require('path');
 
 
-var initializeapi = function(app){
+var initializeapi = function (app) {
 
-	app.get("/home", function (req, res) {
-		res.sendFile(path.join(__dirname ,  '../public/survey.html'))
-	
-		// If no matching route is found default to home
-		app.use(function(req, res){
-			res.sendFile(path.join(__dirname , '../public/home.html'));
-		});
-	
-	
-	});	
+	app.get("/survey ", function (req, res) {
+		res.sendFile(path.join(__dirname, '../public/survey.html'))
+
+
+	});
+
+	// If no matching route is found default to home
+	app.use(function (req, res) {
+		res.sendFile(path.join(__dirname, '../public/home.html'));
+	});
+
+
 }
 
 module.exports = initializeapi;
